9e0c6e6deae8f40464981696f54247fcf28cf023
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-03-20 06:29:06 -0500

    Import skeleton project

---
+++ Quapital iOS Assessment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quapital iOS Assessment
+//
+//  Created by Joe Vargas on 3/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quapital iOS Assessment/Quapital_iOS_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quapital_iOS_AssessmentApp.swift
+//  Quapital iOS Assessment
+//
+//  Created by Joe Vargas on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Quapital_iOS_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
