0e59a434a9b8d4d64060b9efe71b9379ba362f2e
Joe Vargas <joe.vargas.iosdev@gmail.com>
2022-03-23 16:42:44 -0500

    Network call for users api endpoint

--- Quapital iOS Assessment/Manager/NetworkManager.swift
+++ Quapital iOS Assessment/Manager/NetworkManager.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-typealias OnApiSuccess = (Response) -> Void
+typealias OnApiResponseSuccess = (Response) -> Void
+typealias OnApiUserSuccess = (User) -> Void
 typealias OnApiError = (Error) -> Void
 
 class NetworkManager {
@@ -15,7 +16,7 @@ class NetworkManager {
     
     let session = URLSession(configuration: .default)
     
-    func fetchActivities(onSuccess: @escaping OnApiSuccess, onError: @escaping OnApiError) {
+    func fetchActivities(onSuccess: @escaping OnApiResponseSuccess, onError: @escaping OnApiError) {
         let BASE_URL = "https://qapital-ios-testtask.herokuapp.com/activities?from=2021-01-01T00:00:00+00:00&to=2022-01-01T00:00:00+00:00"
         let url = URL(string: BASE_URL)
         
@@ -47,13 +48,50 @@ class NetworkManager {
                         onError(error!)
                     }
                 } catch {
-    //                print("DEBUG: response code: ", response)
                     print("DEBUG: \(error)")
                     onError(error)
                 }
             }
-            
-            
+        }
+        task.resume()
+    }
+    
+    func fetchUserBy(_ id: Int, onSuccess: @escaping OnApiUserSuccess, onError: @escaping OnApiError) -> Void {
+        let BASE_URL = "https://qapital-ios-testtask.herokuapp.com/users/\(id)"
+        let url = URL(string: BASE_URL)
+        
+        let task = session .dataTask(with: url!) { data, response, error in
+            DispatchQueue.main.async {
+                
+                // Handle session error
+                if let error = error {
+                    print("DEBUG: Session error \(error)")
+                    return
+                }
+                
+                // Ensure there is data and a server response
+                guard let data = data, let response = response as? HTTPURLResponse else {
+                    print("DEBUG: Invalid data or response")
+                    return
+                }
+                
+                // Handle response
+                do {
+                    switch response.statusCode {
+                    case 200:
+                        // parse successful result
+                        let users = try JSONDecoder().decode(User.self, from: data)
+                        onSuccess(users)
+                        
+                    default:
+                        // handle unsuccessful error(400s)
+                        onError(error!)
+                    }
+                } catch {
+                    print("DEBUG: \(error)")
+                    onError(error)
+                }
+            }
         }
         task.resume()
     }
